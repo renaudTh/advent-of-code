5f45a2c43eaf10d0ac2f5ad355fd497f8132882d
thomas.renaud <thomas.renaud@belledonne-communications.com>
2022-12-02 10:05:18 +0100

    Add score for second part of day 2

--- day2.c
+++ day2.c
@@ -1,35 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int main()
+{
 
-    FILE * file = fopen("input-d2.txt", "r");
-    if(!file) return EXIT_FAILURE;
-    
-    int score[3][3] = {{4,8,3}, {1,5,9}, {7,2,6}};
+    FILE *file = fopen("input-d2.txt", "r");
+    if (!file)
+        return EXIT_FAILURE;
+
+    int score[3][3] = {{4, 8, 3}, {1, 5, 9}, {7, 2, 6}};
+    int choice[3][3] = {{2,0,1},{0,1,2},{1,2,0}};
     char c = 0;
-    int first;
-    int second;
+    int elf;
+    int you;
+    int your_choice;
+    int second_sum = 0;
     int sum = 0;
-    char gap=0;
-    while((c = fgetc(file)) != EOF){
-        if(c == ' ') continue;           
-        if(c == '\n'){
-            sum+= score[first][second];
+    char gap = 0;
+
+    while ((c = fgetc(file)) != EOF)
+    {
+        if (c == ' ')
             continue;
-        }    
-        if(gap == 0){
-            first = c - 'A';
+        if (c == '\n')
+        {
+            sum += score[elf][you];
+            second_sum += score[elf][your_choice];
+            continue;
+        }
+        if (gap == 0)
+        {
+            elf = c - 'A';
             gap++;
         }
-        else{
-            second = c - 'X';
+        else
+        {
+            you = c - 'X';
+            your_choice = choice[elf][you];
             gap--;
         }
     }
-    printf("your score is %d\n", sum);
-  
+    printf("your score for the first is %d\n", sum);
+    printf("your score for the second is %d\n", second_sum);
+
     return 0;
 }
-
-
